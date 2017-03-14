219f144d2760ae9c312ae03a86ec115d07c24a67
rezalt <ollobrains@gmail.com>
2017-03-14 13:40:17 +0100

    MErged with template

--- src/dk/cphbusiness/heap/PersonHeap.java
+++ src/dk/cphbusiness/heap/PersonHeap.java
@@ -3,7 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package heap;
+package dk.cphbusiness.heap;
+
+import java.util.NoSuchElementException;
+import java.util.PriorityQueue;
 
 /**
  *
